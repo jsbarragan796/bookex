36bd8727959aca2eeecbd352625d4ee4b8a83389
cawolf <camilozambranovotto@hotmail.com>
2018-03-30 17:05:06 -0500

    :tada: Esqueleto básico de react agregado
    Y un par de cambios más :eyes:

---
+++ imports/ui/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { withTracker } from "meteor/react-meteor-data";
+import { Meteor } from "meteor/meteor";
+
+class App extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>Hola mundo!!!! :D</div>
+        );
+    }
+}
+
+
+export default withTracker(()=>{
+    // Meteor.subscribe("algo");
+    return{
+        //las subscripciones.
+    }
+})(App);
